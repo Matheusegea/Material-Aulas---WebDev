7b4717dece2b43f92e6268f80101ff95daf03d64
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-14 20:57:23 -0300

    fazendo o começo do crud

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -18,3 +18,65 @@ let posts = [
         date: "12/10/2023 12:00:00"
     }
 ];
+
+window.onload = function(){
+    mostrarPost();
+    document.querySelector("#postForm").addEventListener("submit", addPost)
+}
+
+//CREATE
+function addPost(infosDoEvento){
+    infosDoEvento.preventDefault()
+
+    //cria variaveis para todos os posts 
+    const textoPost = document.querySelector("#postText").value
+    const categoriaPost = document.querySelector("#postCategory").value
+    const imagemPost = document.querySelector("#postImage").value
+    const dataPost = new Date().toLocaleString()
+    
+    // pega o array de cima e junta com o novoPost
+    const novoPost = {
+        text: textoPost,
+        category: categoriaPost,
+        image: imagemPost,
+        date: dataPost,
+    }
+
+    //deixa o post do mais novo para o mais antigo por conta do unshift
+    posts.unshift(novoPost)
+
+
+    //chama a função mostrarPost para mostrar o novo post
+    mostrarPost()
+}
+//READ
+function mostrarPost(){
+    //pega a id postList
+    const listaPosts = document.querySelector("#postList")
+    listaPosts.innerHTML = ""
+
+    //passa nos items do array
+    posts.forEach(pegaItem => {
+        const cardPost = document.createElement("div")
+        cardPost.classList.add("card")
+
+        cardPost.innerHTML = `
+        <h2>${pegaItem.text}</h2>
+        <img src ="${pegaItem.image}"/>
+        <p>${pegaItem.category}</p>
+        <p>${pegaItem.date}</p>
+        `
+
+        //adiciona o post
+        listaPosts.append(cardPost)
+
+    })
+}
+//UPDATE
+function editarPost(){
+
+}
+//DELETE
+function deletarPost(){
+
+}
