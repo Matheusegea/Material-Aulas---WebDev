b83c9ea70e25d5f6cea39a2930a7594a3146122b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-21 21:07:03 -0300

    Finalizando CRUD

--- Aula 2 - CRUD/Mini-Rede-Social/script.js
+++ Aula 2 - CRUD/Mini-Rede-Social/script.js
@@ -15,13 +15,36 @@ let posts = [
     {
         text: "Este é o terceiro post teste",
         category: "Eventos",
+        image:"https://placedog.net/150?random=3",
         date: "12/10/2023 12:00:00"
     }
 ];
 
 window.onload = function(){
-    mostrarPost();
+    carregarPost()
+    mostrarPost()
     document.querySelector("#postForm").addEventListener("submit", addPost)
+
+    document.querySelector('#postList').addEventListener('click', handleClick)
+
+    localStorage.setItem("nome", "matheus")
+    //console.log(localStorage.getItem("nome"))
+    //localStorage.removeItem("nome")
+    //localStorage.clear()
+}
+
+function handleClick(infosDoEvento){
+
+    const action = infosDoEvento.target.dataset.action
+    const index = infosDoEvento.target.dataset.index
+
+    if(action === "Editar"){
+        editarPost(index)
+    }
+    else if (action === "Apagar"){
+        deletarPost(index)
+    }
+
 }
 
 //CREATE
@@ -44,6 +67,8 @@ function addPost(infosDoEvento){
 
     //deixa o post do mais novo para o mais antigo por conta do unshift
     posts.unshift(novoPost)
+    salvaPost()
+    document.querySelector('#postForm').reset()
 
 
     //chama a função mostrarPost para mostrar o novo post
@@ -56,7 +81,7 @@ function mostrarPost(){
     listaPosts.innerHTML = ""
 
     //passa nos items do array
-    posts.forEach(pegaItem => {
+    posts.forEach((pegaItem, index) => {
         const cardPost = document.createElement("div")
         cardPost.classList.add("card")
 
@@ -65,6 +90,10 @@ function mostrarPost(){
         <img src ="${pegaItem.image}"/>
         <p>${pegaItem.category}</p>
         <p>${pegaItem.date}</p>
+        <button data-action="Editar" data-index="${index}"><i class="fa-solid fa pen-to-square"></i>
+        Editar</button>
+        <button data-action="Apagar" data-index="${index}"><i class="fa-solid fa pen-to-square"></i>
+        Apagar</button>
         `
 
         //adiciona o post
@@ -73,10 +102,33 @@ function mostrarPost(){
     })
 }
 //UPDATE
-function editarPost(){
+function editarPost(index){
+    const postEditado = prompt('Edite o post', posts[index].text)
 
+        posts[index].text = postEditado
+        salvaPost()
+        mostrarPost()
 }
 //DELETE
-function deletarPost(){
+function deletarPost(index){
+    const confirmar = confirm("Tem certeza que deseja excluir essa postagem?")
+    if (confirmar === true){
+        posts.splice(index, 1)
+    }
+    
+    salvaPost()
+    mostrarPost()
 
+}
+
+function salvaPost(){
+    localStorage.setItem("posts", JSON.stringify(posts))
+
+}
+
+function carregarPost(){
+    const postSalvo = localStorage.getItem("posts")
+    if(postSalvo){
+     posts = JSON.parse(postSalvo)
+    }
 }
