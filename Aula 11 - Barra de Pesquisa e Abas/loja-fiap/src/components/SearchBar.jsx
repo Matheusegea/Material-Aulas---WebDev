ec31c94e6f81fb0f4746ba89ba020e7b7fe41d87
matheusegea <matheusegea220504@gmail.com>
2025-10-30 21:01:18 -0300

    finalizando aula 11

--- Aula 11 - Barra de Pesquisa e Abas/loja-fiap/src/components/SearchBar.jsx
+++ Aula 11 - Barra de Pesquisa e Abas/loja-fiap/src/components/SearchBar.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const SearchBar = () => {
+
+    const navigate = useNavigate() // serve para navegar para outra pagina pelo onFocus
+    const [search, setSearch] = useState('')
+    function handleSearch(e){
+        
+        console.log(e.target.value)// serve para mostrar no console tudo o que o usuario esta escrevendo
+        setSearch(e.target.value)
+        navigate(`searchWelcome/${e.target.value}`)// serve para redirecionar o usuario para o que ele esta escrevendo na barra de pesquisa utilizando e.target.value mostrando na url 
+    }
 
     return (
          <div className="w-64">
@@ -6,6 +18,10 @@ const SearchBar = () => {
             type="text"
             id="search"
             placeholder="Pesquisar produtos..."
+            onChange = {handleSearch} // serve para mostrar a mudança na função "handleSearch"
+            onFocus = {() => navigate("searchWelcome")}// serve para navegar para a pagina searchWelcome
+            onBlur = {() => navigate("/")}// serve para quando nao estiver no foco da barra de pesquisa voltar para a home page
+            value = {search}// serve para modificar o value com o search utilizando o useState
             className="pl-10 pr-3 py-1 w-64 bg-transparent border-b-2 border-black focus:border-black outline-none transition-colors duration-300"
           />
 
--- Aula 11 - Barra de Pesquisa e Abas/loja-fiap/src/pages/SearchResults.jsx
+++ Aula 11 - Barra de Pesquisa e Abas/loja-fiap/src/pages/SearchResults.jsx
@@ -1,7 +1,37 @@
+import { useParams } from "react-router-dom"; // serve para usar os parametros da url
+import { useEffect, useState } from "react";
+import SectionContainer from "../components/SectionContainer";
+import ProductCard from "../components/ProductCard";
+
 const SearchResults = () => {
+
+    const {termoBusca} = useParams() // sempre deve guardar a execução em uma variavel, sempre desestruturar o parametro 
+    const [products, setProducts] = useState([]);
+    const API_URL = import.meta.env.VITE_API_URL;
+    
+      useEffect(() => {
+        const fetchProduct = async () => {
+          const res = await fetch(`${API_URL}`);
+          const data = await res.json();
+          setProducts(data);
+        };
+        fetchProduct();
+      }, []);
+
+      const produtosFiltrados = products.filter(pegaItem => pegaItem.title.toLowerCase().includes(termoBusca.toLowerCase())) // serve para filtrar os produtos pelo titulo deles, tbm deixa em letra minuscula
+
+      if(!produtosFiltrados){ // if para mostrar uma mensagem se nao encontrou nenhum produto
+        return <p className="text-center">Nenhum produto encontrado </p>
+      }
+
     return (
         <div>
-            <h1 className="text-2xl font-bold">Resultados da Pesquisa para: </h1>
+            <h1 className="text-2xl font-bold">Resultados da Pesquisa para: {termoBusca}</h1>
+            <SectionContainer tittle="Resultados">
+            {produtosFiltrados.map(pegaItem => 
+                <ProductCard {...pegaItem}/>
+            )}
+            </SectionContainer>
         </div>
     );
 };
